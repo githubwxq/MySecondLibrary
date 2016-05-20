cc63965771a6c2afa16dcf2983bc04f9612d2190
805380422@qq.com <805380422@qq.com>
2016-05-20 14:13:51 +0800

    这是我提交的第二个库文件啦啦啦

---
+++ secondlibrary/src/main/java/com/example/wxq/secondlibrary/SecondUtils.java
@@ -0,0 +1,12 @@
+package com.example.wxq.secondlibrary;
+
+/**
+ * Created by Administrator on 2016/5/20.
+ */
+public class SecondUtils {
+    public static String  getSecondLibray(){
+
+        return "我是第二个库文件";
+
+    }
+}
